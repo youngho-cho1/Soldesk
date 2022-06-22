c022b073999f962adbc674f4218cef97f93c5ec3
youngho <mmy4637@naver.com>
2022-06-22 09:46:47 +0900

    Create GlobalParams.py

---
+++ Python/machine/NLP/chatbot/config/GlobalParams.py
@@ -0,0 +1,13 @@
+# 단어 시퀀스 벡터 크기
+MAX_SEQ_LEN = 15
+
+def GlobalParams():
+    global MAX_SEQ_LEN
+
+DB_HOST = "127.0.0.1"
+DB_USER = "pyuser"
+DB_PASSWORD = "1234"
+DB_NAME = "chatbot"
+
+def DatabaseConfig():
+    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
